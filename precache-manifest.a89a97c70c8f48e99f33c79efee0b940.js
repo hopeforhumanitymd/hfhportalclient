a225d2ca9972e11d4c4eed293fbe36790fb21f69
tinu4 <tinuvanapamula@gmail.com>
2020-08-30 19:35:41 -0400

    Updates

---
+++ precache-manifest.a89a97c70c8f48e99f33c79efee0b940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c308b6e08b688afcaabdd1f26a4a79a7",
+    "url": "/hfhportalclient/index.html"
+  },
+  {
+    "revision": "c2cd81796ed66dc3f51e",
+    "url": "/hfhportalclient/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3081f1bd366c5014f3ed",
+    "url": "/hfhportalclient/static/js/2.43c131ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hfhportalclient/static/js/2.43c131ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2cd81796ed66dc3f51e",
+    "url": "/hfhportalclient/static/js/main.da6fb726.chunk.js"
+  },
+  {
+    "revision": "0b74b6b2533b75f005e6",
+    "url": "/hfhportalclient/static/js/runtime-main.daa565cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hfhportalclient/static/media/logo.5d5d9eef.svg"
+  }
+]);
